2ffb6ed1793ea5053ec0459a95e1e234e4b7ec36
Siarhei <taler34@gmail.com>
2021-06-13 22:42:28 +0300

    sockpairs

---
+++ SockPairs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SockPairs
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+               Console.WriteLine(SockPairs("CABBACCC"));
+        }
+
+        public static int SockPairs(string socks)
+        {
+            char[] charArray = socks.ToCharArray();
+
+            Dictionary<char, int> charDisctionary = new Dictionary<char, int>();
+
+            foreach (char ch in charArray)
+            {
+                if (charDisctionary.ContainsKey(ch))
+                {
+                    charDisctionary[ch] = charDisctionary[ch] + 1;
+                }
+                else
+                {
+                     charDisctionary.Add(ch, 1);
+                }
+
+            }
+
+              int result = 0;
+
+               foreach ( int count in charDisctionary.Values)
+               {
+                if (count > 1)
+                {
+                    result += count / 2;
+                }
+               }
+               return result;
+        }
+    }
+    }
+
